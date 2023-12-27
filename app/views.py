eb54b500c286679fc3998703fdd165ee88179e2a
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-12-28 00:16:46 +0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -630,7 +630,7 @@ class MakeBooking(View):
         self.driver.get(self.url)
         self.set_cookie()
         page_source = self.driver.page_source
-        return JsonResponse(page_source)
+        return JsonResponse({"source":page_source})
         # Define a regular expression pattern for UUIDs
         #uuid_pattern = re.compile(r'data-widget-uuid="([^"]+)"')
 
